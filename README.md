5adf8dabc10216be0a920c30e80cad880e3276bb
TheJalpa <30740021+TheJalpa@users.noreply.github.com>
2020-09-01 17:14:31 -0700

    updating with lottery function

--- README.md
+++ README.md
@@ -17,3 +17,23 @@ This will send the tagged user random words of affirmation privately
 * !encourage
 
 The bot will post a random inspirational quote to the channel
+
+* !lottery
+
+Feeling generous?  Want to hand out some free keys to users from steam?
+Do you want to make sure it doesn't accidentally make people fight over them?
+Want to make sure people don't get locked out?
+Well, every time someone does "!lottery" it will private message them a key.
+It will then add them to the lotteryclaims.txt file.
+keysclaimed.txt will then be updated, and moved from keysactive.txt.
+
+So simply add any active keys to keysactive.txt.  The lottery will randomly pluck one,
+give it to a user, add that key to the list of used keys, and users claimed, and
+now everyone has a chance to win the lottery!
+
+If you want to truly make it even more random where maybe they do or don't get one
+you can add bogus fake keys or messages in the keysactive.txt as it literally just cuts
+and pastes the contents from one file to the other.
+
+Want 10 keys and 100 lines?  Put them in the first 10 lines and then have 90 lines of
+lorem ipsum.  Be creative, have fun.
--- happybot.py
+++ happybot.py
@@ -6,23 +6,68 @@ def random_line(fname):
     return random.choice(lines)
 
 client = discord.Client()
-
 @client.event
 async def on_message(message):
     message.content.lower()
     if message.author == client.user:
         return
+    if message.content.startswith("!lottery"):
+        user = message.author
+        convuser = str(user)
+        channel = await user.create_dm()
+        with open("src/keysactive.txt", "r+") as f1, open("src/keysclaimed.txt", "r+") as f2, open("src/lotteryclaims.txt", "r+") as f3:
+            f3lines = f3.readlines()
+            f3.seek(0)
+            badbool = False
+            for line in f3lines:
+                if line.startswith(convuser):
+                    await channel.send("It looks like you've recently claimed a key from the lottery.  Please wait for an administrator to clear the lottery.")
+                    badbool = True
+                    break
+            for line in f3.readlines():
+                f3.seek(0)
+                if badbool == True:
+                    break
+                if not line.startswith(convuser):
+                    print("bruh")
+                    f1lines = f1.readlines()
+                    f2lines = f2.readlines()
+                    randomline = random_line('src/keysactive.txt')
+                    f1.seek(0)
+
+                    for line in f1lines:
+                        if not line.startswith(randomline):
+                            f1.write(line)
+                    for line in f2lines:
+                        if line.startswith("\n"):
+                            f2.write(randomline + "\n")
+                            f3.seek(0)
+                            data = f3.read(100)
+                            if len(data) > 0 :
+                                f3.write("\n")
+                                f3.write(convuser)
+                                f3.truncate()
+                    f1.truncate()
+                    f2.truncate()
+                    
+
+                    await channel.send("Wow, looks like someone got a steam key! " + randomline)
+                    break
+
     if message.content.startswith("!encourage"):
         await message.channel.send(random_line('src/inspire.txt'))
     if message.content.startswith("!kindwords"):
-        await message.channel.send("{0.author.mention}".format(message) + random_line(" " + 'src/affirmation.txt'))
+        await message.channel.send("{0.author.mention.}".format(message) + random_line(" " + 'src/affirmation.txt'))
     if message.content.startswith("!sendkindwords"):
-        user = message.mentions[0]
+        user = user = message.mentions[0]
+        print (user)
         uname = message.mentions[0].mention
         channel = await user.create_dm()
-        botaction = random_line('botaction.txt')
-        await channel.send("Hey there " + uname + ", it looks like a friend wanted to send you something positive.\n" + botaction + "\n" + random_line('src/affirmation.txt'))
+        botaction = random_line('src/botaction.txt')
+        await channel.send("Hey there " + uname + ", it looks like " + "{0.author.mention}".format(message) + " wanted to send you something positive.\n" + botaction + "\n" + random_line('src/affirmation.txt'))
 #" + "{0.author.mention}".format(message) + "
+
+
 
 #Where the client id needs to be
 #You can get this from your discord developer portal under your Bot page and add your token.
